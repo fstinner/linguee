ef6ec6bbdacfab1da0f58f55a7707cbdd92b329a
florian.stinner <fstinner@eonerc.rwth-aachen.de>
2020-08-20 00:52:39 +0200

    add functional version of linguee2.py

--- linguee2.py
+++ linguee2.py
@@ -1,10 +1,12 @@
 # FR-EN translator using multiple methods
 
 import sys
+import os
+
 try:
-	from urllib.request import urlopen
+    from urllib.request import urlopen
 except ImportError:
-	from urllib2 import urlopen
+    from urllib2 import urlopen
 try:
     import simplejson as json
 except (ImportError):
@@ -12,178 +14,108 @@ except (ImportError):
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 import string
-from wiktionaryparser import WiktionaryParser
+# from wiktionaryparser import WiktionaryParser
 import requests
 
-parser = WiktionaryParser()
-parser.set_default_language('french')
-wordie = quote(sys.argv[1])
+DICT_LANGUAGE = {"de": "German",
+                 "en": "English",
+                 "fr": "French",
+                 "es": "Spanish",
+                 "pt": "Portuguese",
+                 "it": "Italian",
+                 "ru": "Russian",
+                 "ja": "Japanese",
+                 "zh": "Chinese",
+                 "pl": "Polish",
+                 "nl": "Dutch",
+                 "sv": "Swedish",
+                 "da": "Danish",
+                 "fi": "Finnish",
+                 "el": "Greek",
+                 "cs": "Czech",
+                 "ro": "Romanian",
+                 "hu": "Hungarian",
+                 "sk": "Slovak",
+                 "bg": "Bulgarian",
+                 "sl": "Slovene",
+                 "lt": "Lithuanian",
+                 "lv": "Latvian",
+                 "et": "Estonian",
+                 "mt": "Maltese"
+                 }
 
-def local (word): # search through the wiktionary dumps
-	word = word.lower()
-	try:
-		file1 = open("dictionary/"+word[:1]+".txt", "r")
-		for string in file1:
-			collection = string.split(";")
-			if (collection[0] == word):
-				answer = collection[3] # need to learn how to send an array of definitions & have html handle them
-				file1.close()
-				return answer
-		file1.close()
-		answer = "word not found"
-		return answer
-	except:
-		answer = "word not found"
-		return answer
-
-def wiki1 (word): # search using WiktionaryParser
-	word = word.lower()
-	wikiAns = parser.fetch(word)
-	if (wikiAns != []):
-		try:
-			output = wikiAns[0]['definitions'][0]['text']
-			responses = output.split("\n")
-			answer = responses[1]
-			file2 = open("dictionary/"+word[:1]+".txt", "a")
-			file2.write(word+";X;TR-FR-EN;"+answer+";X;\n")
-			file2.close()
-			return answer
-		except IndexError:
-			answer = "word not found"
-			return answer
-	answer = "word not found"
-	return answer
-
-def wiki2 (word):
-
-#
-# search through wiktionaryparser for the past participle or pronoun.
-# will need to modify for new classes of words when lookup fails.
-#
-# pending: testing
-#
-	word = word.lower()
-	wikiLink = "https://en.wiktionary.org/w/api.php?format=json&action=query&titles="+quote(word)+"&rvprop=content&prop=revisions&redirects=1" 
-	wikii = requests.get(wikiLink)
-	wikiJSON = wikii.json()
-	try:
-		for pageId in wikiJSON["query"]["pages"]:
-			output = wikiJSON["query"]["pages"][pageId]["revisions"][0]["*"]
-		arr = output.split("----")
-		lookAt = []
-		for y in arr:
-			if "=French=" in y:
-				lookAt.append(y)
-
-		if len(lookAt) > 0:
-			temp = lookAt[0].split("\n\n")
-			lookAt = temp
-		else:
-			toReturn = "word not found"
-			return toReturn
-
-		definitions = []
-		for x in lookAt:
-			if x.startswith("#"):
-				transform = ['#', "{", "}", "[", "]", "'", ":"]
-				transform2 = ["|"]
-				translator = str.maketrans({key: None for key in transform})
-				translator2 = str.maketrans({key: " " for key in transform2})
-				x = x.translate(translator)
-				x = x.translate(translator2)
-				definitions.append(x[1:])
-				definitions = definitions[0].split("\n")
-
-		if (checkPP(definitions[0]) != "nothing"):
-			toReturn = checkPP(definitions[0])
-			#print toReturn
-		else:
-			toReturn = definitions[0]
-	except IndexError:
-		toReturn = "word not found"
-	
-	return toReturn
-
-def checkPP(word): # find definition of the past participle or forms of adjectives
-	options = [' singular of ', ' plural of ', ' past participle of ']
-
-	for d in options:
-		if d in word:
-			arr = word.split(d)
-			arr1 = arr[1].split(" ")
-			tryThis = local(arr1[0])
-			if(tryThis != "word not found"):
-				return tryThis
-	return "nothing"
-
-def linguee(word):
-
-#
-# crawl through linguee for a definition.
-# will create new functions for API calls to Collins, Oxford, Glosbed etc when I get API keys for them approved
-#
-# will rely on Glosbed as a secondary look-up service, with Collins next and Linguee as a last option.
-#
-	word = word.lower()
-	lingueeLink = "http://www.linguee.com/english-french/search?source=auto&query=" + quote(word)
-	page = urlopen(lingueeLink)
-	initial = BeautifulSoup(page, "html.parser")
-	definition = initial.find('a', class_="dictLink featured")
-
-	if definition is not None:
-		definition = initial.find('a', class_="dictLink featured").get_text()
-		file2 = open("dictionary/"+word[:1]+".txt", "a")
-		file2.write(word+";X;TR-FR-EN;"+definition+";X;\n")
-		file2.close()
-	else:
-		definition = "word not found"
-	return definition + " [l]"
+TYPES = {"noun": ["noun, masculine", "noun, neuter", "noun, feminine", "noun", 'noun,\xa0masculine', "noun,\xa0neuter",
+                  "noun,\xa0feminine"],
+         "adjective": ["adjective"],
+         "verb": ["verb"]}
 
 
-def tryDefine(word):
+def linguee(word, from_l, to_l, type=None):
+    #
+    # crawl through linguee for a definition.
+    # will create new functions for API calls to Collins, Oxford, Glosbed etc when I get API keys for them approved
+    #
+    # will rely on Glosbed as a secondary look-up service, with Collins next and Linguee as a last option.
+    #
+    word = word.lower()
+    from_l = DICT_LANGUAGE[from_l].lower() if len(from_l) == 2 else from_l.lower()
+    to_l = DICT_LANGUAGE[to_l].lower() if len(to_l) == 2 else to_l.lower()
 
-	localSearch = local(word) # METHOD 1 = Look through Wiktionary dumps
+    short_from = from_l if len(from_l) == 2 else list(DICT_LANGUAGE.keys())[
+        list(DICT_LANGUAGE.values()).index(from_l.capitalize())]
+    short_to = to_l if len(to_l) == 2 else list(DICT_LANGUAGE.keys())[
+        list(DICT_LANGUAGE.values()).index(to_l.capitalize())]
 
-	if localSearch != "word not found":
-		return localSearch
+    linguee_link = "http://www.linguee.com/{}-{}/search?source=auto&query={}".format(from_l, to_l, quote(word))
+    page = urlopen(linguee_link)
+    soup = BeautifulSoup(page, "lxml")
 
-	parserSearch = wiki1(word) # METHOD 2 = Look through Wiktionary using JSON parser
+    definitions = list()
 
-	if parserSearch != "word not found":
-		return parserSearch
+    def1 = soup.find_all('a', class_="dictLink featured")
+    for element in def1:
+        if bool(element.find_parent(class_='lemma_content')) * bool(element.find_parent(attrs={
+                "data-source-lang": short_from.upper()})):
+            if bool(type) and any(bool(element.parent.find(attrs={"title": t})) for t in TYPES[type]):
+                definitions.append(element.get_text())
+            elif not bool(type):
+                definitions.append(element.get_text())
 
-	wikiSearch = wiki2(word) # METHOD 3 = Look through Wiktionary dumps for nuances like participles and singulars
+    def2 = soup.find_all('a', class_="dictLink")
+    for element in def2:
+        if bool(element.find_parent(class_='lemma_content')) * bool(element.find_parent(
+                attrs={"data-source-lang": short_from.upper()})) * bool(element.find_parent(
+                class_='translation_group')) * bool(element.find_parent(class_='exact')):
+            if bool(type) and any(bool(element.parent.find(attrs={"title": t})) for t in TYPES[type]):
+                definitions.append(element.get_text())
+            elif not bool(type):
+                definitions.append(element.get_text())
+    directory = "dictionary"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-	if wikiSearch != "word not found":
-		return wikiSearch
+    writepath = r"{}/{}.txt".format(directory, word)
+    mode = 'a+' if os.path.exists(writepath) else 'w+'
 
-	return linguee(word) # METHOD 4 = crawl through Linguee as a final recourse
+    if definitions is not None:
+        with open(writepath, mode) as file2:
+            file2.write("{};TR-{}-{};{};\n".format(word, short_from, short_to, definitions))
+            file2.close()
+    else:
+        definitions = "word not found"
+    return definitions
 
 
+if __name__ == "__main__":
+    lang_from = "de"
+    lang_to = "en"
+    key = "Messwert"
 
-#whatever function -- vanity only
+    definitions1 = linguee(key, lang_from, lang_to, "noun")
+    definitions2 = list()
+    for definition in definitions1:
+        print("{},{},{}".format(definition, lang_to, lang_from))
+        definition_ = linguee(definition, lang_to, lang_from, "noun")
+        definitions2.append(definition_)
 
-def genExample():
-	lingueeLink = "http://www.linguee.com/english-french/search?source=auto&query=" + quote(word)
-	page = urlopen(lingueeLink)
-	initial = BeautifulSoup(page, "html.parser")
-	example = initial.find('table', class_="result_table")
-
-	# left[0] in french ~ right[0] in english
-
-	left = [] # the french sentence array
-	right = [] # the corresponding english sentence array
-
-	for row in example.findAll("tr"):
-		lefty = row.find('td', class_="sentence left")
-		link1 = (lefty.find('div', class_="source_url_spacer")).get_text()
-		righty = row.find('td', class_="sentence right2")
-		link2 = (righty.find('div', class_="source_url_spacer")).get_text()
-		left.append((lefty.get_text()).replace("\n", " ").replace(link1,"").replace("\r", ""))
-		right.append((righty.get_text()).replace("\n", " ").replace(link2,"").replace("\r", ""))
-
-	sendExample = [left[0], right[0]] # send random examples? need to repurpose for context-based searches later.
-
-	return sendExample
-
-tryDefine(wordie)
+    print(definitions2)
